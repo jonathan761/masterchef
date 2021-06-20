055c92fc4f2135830dde4a717f5aaa897b85b954
jonathan761 <75612410+jonathan761@users.noreply.github.com>
2021-06-20 15:32:32 +0530

    Add files via upload

---
+++ receipe.js
@@ -0,0 +1,48 @@
+ 
+STEP 1: PREPARE THE DOUGH
+In a large mixing bowl, sift 2 cups all-purpose flour, 1/2 teaspoon baking powder, 1/4 teaspoon baking soda, add 1/2 teaspoon sugar & 1/2 teaspoon salt. Mix well. Add 1/2 cup yogurt & 2 tablespoon oil. Mix well.
+
+
+
+
+Add 1/2 cup warm milk/water and knead until you achieve a soft and elastic dough. If the dough is too sticky, grease your palms with oil and continue kneading. Avoid adding more flour, 1-2 tablespoon extra flour is alright.
+
+Kulcha Recipe On Tawa | How To Make Kulcha (No Yeast)
+
+
+
+
+STEP 2: PROOF THE DOUGH
+Transfer the dough to a greased bowl, drizzle some oil over the dough, spread it evenly to avoid it from drying out. Cover with a damp cloth and set aside for 2-3 hours minimum or about 6-8 hours. I usually let it ferment overnight, which results in a soft and fluffy kulcha.
+
+Kulcha Recipe On Tawa | How To Make Kulcha (No Yeast)
+
+
+
+
+STEP 3: DIVIDE THE DOUGH
+After the dough has fermented, you will see it has leavened and risen a bit. Knead the dough for another 5 minutes. Divide the dough into 6 equal size balls.
+
+Kulcha Recipe On Tawa | How To Make Kulcha (No Yeast)
+
+
+
+
+Kulcha Recipe On Tawa | How To Make Kulcha (No Yeast)
+
+STEP 4: ROLL OUT THE DOUGH
+Now dust the working surface with flour, take one ball, dust it in flour.
+For butter kulchas: Roll the floured ball to about 4 inches in diameter, add some cilantro & black sesame seeds, roll the ball to about 7-8 inches in diameter, and 1/8 inch thickness. Dust off excess flour.
+
+For garlic kulchas: Add some cilantro, grated garlic & black sesame seeds, roll the ball to about 7-8 inches in diameter, and 1/8 inch thickness. Dust off excess flour.
+STEP 5: ROAST THE KULCHA ON THE TAWA OR A CAST-IRON SKILLET
+ 
+
+Heat an iron skillet/tawa, place the kulcha, cilantro side up on the greased tawa, cover with a lid and cook for 45 seconds to 1 minute on medium heat, once brown spots appear at the bottom, and bubbles appear on top, flip and roast the other side for 45 seconds. 
+
+You may directly roast the other side over the flame by inverting the skillet upside down until you get charred spots. In that case, make sure you dab some water on the plain side without the cilantro, etc and cook that first, then invert the skillet and cook the other side.
+STEP 6: DAB SOME BUTTER OR GHEE
+Brush the flatbread with ghee/butter roast on both sides till fully cooked through, flipping 2-3 times more. Prepare the remaining kulchas in the same manner. Serve with yogurt & pickle or with curry of your choice.
+
+s
+
